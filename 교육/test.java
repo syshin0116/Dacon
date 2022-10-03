ecf367b74b4c5766c1432f4a43872de029d41408
syshin0116 <syshin0116@gmail.com>
2022-10-04 01:23:01 +0900

    유동인구

--- 교육/test.java
+++ 교육/test.java
@@ -2,13 +2,12 @@ public class test{
     public static void main(String[] args){
         // 세로줄
         System.out.println("세로줄:");
-
         for(int i = 1; i <8; i++){
             int j = 8-i;
             System.out.println(i + "\t" + j + "\t" + i + "\t" + j + "\t" + i + "\t" + j + "\t" + i);
         }
 
-        // 가로줄
+        // 가로줄 
         System.out.println("\n가로줄:");
         String row1 = "";
         String row2 = "";
@@ -16,13 +15,10 @@ public class test{
              row1 += k + "\t";
              row2 += 8-k + "\t";
         }
-        System.out.println(row1);
-        System.out.println(row2);
-        System.out.println(row1);
-        System.out.println(row2);
-        System.out.println(row1);
-        System.out.println(row2);
-        System.out.println(row1);
-
+        for (int m = 0; m<3;m++){
+            System.out.println("row1>>\t" + row1);
+            System.out.println("row2>>\t" + row2);
+        }
+        System.out.println("row1>>\t" + row1);
     }
 }
